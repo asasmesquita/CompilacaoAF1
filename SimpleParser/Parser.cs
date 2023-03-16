3384259c26f477b0191fc0145e8bc476a0dfe1ae
asasmesquita <1e9pql1c7@mozmail.com>
2023-03-16 09:54:15 +0100

    Simple parser running for base use case

---
+++ SimpleParser/Parser.cs
@@ -0,0 +1,54 @@
+namespace SimpleParser;
+
+class Parser{
+    private static StreamReader _streamReader;
+    private int lookahead;
+    internal Parser(StreamReader streamReader){
+        _streamReader = streamReader;
+        if(!_streamReader.EndOfStream){
+            lookahead = _streamReader.Read();
+        }
+    }
+
+    internal void Expression(){
+        while(!_streamReader.EndOfStream){
+            Term();
+            while(true){
+                if(lookahead == '+'){
+                    Match('+');
+                    Term();
+                    Console.Write('+');
+                }
+                else if(lookahead == '-'){
+                    Match('-');
+                    Term();
+                    Console.Write('-');
+                }
+                else{
+                    return;
+                }
+            }
+        }
+    }
+
+    private void Term(){
+        if(Char.IsDigit(((char)lookahead))){
+            Console.Write((char)lookahead);
+            Match(lookahead);
+        }
+        else{
+            throw new Exception("Syntax error");
+        }
+    }
+
+
+    private void Match(int token){
+        if(lookahead == token){
+            lookahead = _streamReader.Read();
+        }
+        else{
+            throw new Exception("Syntax error");
+        }
+
+    }
+}
---
+++ SimpleParser/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace SimpleParser;
+
+class Program{
+    static void Main(string[] args){
+        if(args.Length == 1){
+            try{
+                using(FileStream stream = File.Open(args[0], FileMode.Open, FileAccess.Read, FileShare.None)){
+                    try{
+                        StreamReader reader = new StreamReader(stream);
+                        Parser parser = new Parser(reader);
+                        parser.Expression();
+
+                        Console.WriteLine();
+                        Console.WriteLine("Operation terminated with success");
+                    }
+                    catch(Exception e){
+                        Console.WriteLine("Error: cannot open file");
+                        Console.WriteLine(e.Message);
+                    }  
+                }
+            }
+            catch(Exception e){
+                Console.WriteLine("Error: file not found");
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+        else{
+            Console.WriteLine("Error: mssing argument, insert file to compile");
+            Console.WriteLine("Usage: simpleparser [file with code to compile]");
+        }
+    }
+}
+
